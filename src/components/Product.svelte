cb18f49370836b4018d7c49241be929192ed31ed
Alexandros Xanthopoulos <xanthopoulosa22@gmail.com>
2024-03-14 18:40:45 +0200

    gfhfgh

---
+++ src/components/Product.svelte
@@ -0,0 +1,13 @@
+<h1>title</h1>
+<p>my book</p>
+
+<style>
+    p {
+        color: red;
+
+    }
+
+    h1 {
+        color: green;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
+<script>
+	import Product from "../components/Product.svelte";
+
+</script>
 <h1>This is a working project.</h1>
+
+<Product/>
